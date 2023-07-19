a13eb76616d9a19a4dac3f1bf1dc92adce809d0c
Manel-Bel <manelbelguenbour@gmail.com>
2023-07-19 15:10:01 +0200

    home page done

---
+++ front/src/pages/Poste.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { preview } from '../assets';
+import {getRandomPrompt} from '../utils';
+import { Form, Loader } from '../composants';
+const Poste = () => {
+  return (
+    <div>Poste</div>
+  )
+}
+
+export default Poste
---
+++ front/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./home";
+import Poste from "./Poste";
+
+
+export {
+    Home,
+    Poste
+}
